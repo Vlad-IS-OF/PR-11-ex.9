03e36dbff8c422a250e75d637a96311ae47ea185
Vlad-IS-OF <Svlad9231@gmail.com>
2023-03-11 23:08:06 +0500

    Initial commit

---
+++ Point.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_11_ex._9
+{
+    public class Point
+    {
+        /*Унарные операции:
+        ++  увеличить координаты x на 1,
+        -- уменьшение координаты х на 1.
+
+        Операции приведения типа:
+        int (явная) – результатом является целая часть координаты х;
+        double (неявная) – результатом является координата y.
+
+        Бинарные операции:
+        +  Point p – вычисляется расстояние до точки p. Результатом должно быть вещественное число. 
+        + целое число (лево- и право- сторонние операции). Увеличивается координата х, результатом является объект типа Point*/
+        //Параметры
+        public double x;
+        public double y;
+        
+        //Конструктор
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+        
+        //Деконструктор
+        ~Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        //Функции
+        public double length(Point tochka2)
+        {
+            return Math.Abs(Math.Sqrt((tochka2.x-this.x)+(tochka2.y-this.y)));
+        }
+        public double length(double x, double y)
+        {
+            return Math.Abs(Math.Sqrt((x-this.x)+(y-this.y)));
+        }
+        public void Show()
+        {
+            Console.WriteLine($"X:{this.x}; Y:{this.y};");
+        }
+        public string toString()
+        {
+            return $"X:{this.x}; Y:{this.y};";
+        }
+        //Перегрузка
+        /*
+         *      Унарные операции:
+         * ++  увеличить координаты x на 1,+
+         * -- уменьшение координаты х на 1
+         */
+        public static Point operator ++(Point p) { 
+            p.x++;
+            return p;
+        }
+        public static Point operator --(Point p) { 
+            p.x--;
+            return p;
+        }
+        /*
+         * Операции приведения типа:
+         * int (явная) – результатом является целая часть координаты х;
+         * double (неявная) – результатом является координата y.
+         */
+        public static implicit operator double(Point p) {return p.y;}
+        public static explicit operator int(Point p) {return (int)p.x;}
+
+        /*
+         * Бинарные операции:
+         * +  Point p – вычисляется расстояние до точки p. Результатом должно быть вещественное число
+         * + целое число (лево- и право- сторонние операции). Увеличивается координата х, результатом является объект типа Point
+         */
+        public static double operator +(Point p1, Point p2) { 
+            return p1.length(p2);
+        }
+        public static Point operator +(Point p,double x)
+        {
+            p.x += x;
+            return p;
+        }
+        public static Point operator +(double x, Point p)
+        {
+            p.x += x;
+            return p;
+        }
+        public static Point operator -(Point p, double x)
+        {
+            p.x -= x;
+            return p;
+        }
+        public static Point operator -(double x, Point p)
+        {
+            p.x -= x;
+            return p;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_11_ex._9
+{
+    internal class Program
+    {
+        static double GetDouble(String str)
+        {
+
+            while (true)
+            {
+                Console.Write(str);
+                if (Double.TryParse(Console.ReadLine(), out double num))
+                {
+                    return num;
+                }
+            }
+        }
+        static void allend()
+        {
+            Console.Write("Нажмити любую кнопку чтобы продолжить ...");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            //Инициализация 2 точек
+            Point A = new Point();
+            Point B = new Point();
+
+            //Заполнение Точек
+            Console.WriteLine("Введите начальные координаты точки A:");
+            A.x = GetDouble("X:");
+            A.y = GetDouble("Y:");
+            Console.Clear();
+            Console.WriteLine("Введите начальные координаты точки B:");
+            B.x = GetDouble("X:");
+            B.y = GetDouble("Y:");
+
+            //Расчёт растояния между точками
+            Console.WriteLine($"Координаты точки A: {A.toString()} Координаты точки B: {B.toString()}");
+            Console.WriteLine($"Растояние между точками \"{A.x};{A.y}\" и \"{B.x};{B.y}\" = {string.Format("{0:f2}", A.length(B))}");
+            allend();
+        start:
+            Console.Clear();
+            int chois = -1;
+            while (((chois < 0) || (chois > 3)))
+            {
+                Console.WriteLine("Какое задание вы хотите выполнить:\n1) Унарные операции (++ || --)\n2) Приведения типа\n3) Бинарные операции\n0) Заверить работу;");
+                chois = (int)GetDouble("Я вибераю: ");
+            }
+            switch (chois)
+            {
+                case 0:
+                    return;
+                case 1:
+                    Console.WriteLine($"Координаты точки A до изменений (A++): {A.toString()}");
+                    A++;
+                    Console.WriteLine($"Координаты точки A после изменений (A++): {A.toString()}");
+                    allend();
+                    Console.Clear();
+                    Console.WriteLine($"Координаты точки A до изменений (A--): {A.toString()}");
+                    A--;
+                    Console.WriteLine($"Координаты точки A после изменений (A--): {A.toString()}");
+                    allend();
+                    break; 
+                case 2:
+                    Console.WriteLine($"Координаты точки A: {A.toString()}");
+                    Console.WriteLine($"Преобразование точки A int(явная) – результатом является целая часть координаты х: {(int)A}");
+                    Console.WriteLine($"Преобразование точки A double (неявная) – результатом является координата y: {(double)A}");
+                    allend();
+                    break;
+                case 3:
+                    Console.WriteLine($"Координаты точки A: {A.toString()} Координаты точки B: {B.toString()}");
+                    Console.WriteLine($"А + B = Растояние = {string.Format("{0:f2}", A + B)}\nA + 4 = A.X + 4 = {(A + 4).toString()}\n4 + A = 4 + A.X = {(4 + A).toString()}");
+                    Console.WriteLine($"A - 4 = A.X - 4 = {(A - 4).toString()}\n4 - A = 4 - A.X = {(4 - A).toString()}");
+                    allend();
+                    break;
+
+            }
+            goto start;
+        }
+    }
+}
